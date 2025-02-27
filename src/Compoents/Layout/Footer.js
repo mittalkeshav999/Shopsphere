0bf717f06dd89f3c3746ffd96992e3105ce47b26
Keshav Mittal <89253638+mittalkeshav999@users.noreply.github.com>
2025-02-28 03:21:56 +0530

    Footer Updated

--- src/Compoents/Layout/Footer.js
+++ src/Compoents/Layout/Footer.js
@@ -1,94 +1,92 @@
 import React from "react";
-import "./Footer.css";
 import { FaAppStore, FaFacebook, FaYoutube, FaInstagram, FaGooglePlay, FaTwitter } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { useTranslation } from "../Translation/TranslationContext";
 
 const Footer = () => {
-  const { t } = useTranslation(); // Translation function
+  const { t } = useTranslation(); 
+
+  const footerSections = [
+    {
+      title: `${t("ONLINE")} ${t("SHOPPING")}`,
+      links: [
+        <Link className="text-decoration-none text-dark" to="/categorypage/Mobiles">{t("Mobiles")}</Link>,
+        <Link className="text-decoration-none text-dark" to="/categorypage/Electronics">{t("Electronics")}</Link>,
+        <Link className="text-decoration-none text-dark" to="/categorypage/Sportswear">{t("Sportswear")}</Link>,
+        <Link className="text-decoration-none text-dark" to="/categorypage/Home Appliances">{`${t("Home")} ${t("Appliances")}`}</Link>, 
+        <Link className="text-decoration-none text-dark" to="/categorypage/Clothing">{t("Clothing")}</Link>
+        ],
+    },
+    {
+      title: `${t("CUSTOMER")} ${t("POLICIES")}`,
+      links: [
+        <Link className="text-decoration-none text-dark" to="/contactus">{`${t("Contact")} ${t("Us")}`}</Link>,
+        t("FAQ"),
+        `${t("Terms")} & ${t("Conditions")}`,
+        `${t("Terms")} ${t("Of")} ${t("Use")}`,
+        `${t("Track")} ${t("Orders")}`,
+        t("Shipping"),
+        t("Cancellation"),
+        t("Returns"),
+        `${t("Privacy")} ${t("Policy")}`,
+        `${t("Grievance")} ${t("Redressal")}`,
+      ],
+    },
+    {
+      title: `${t("USEFUL")} ${t("LINKS")}`,
+      links: [t("Blog"), t("Careers"), `${t("Site")} ${t("Map")}`, `${t("Corporate")} ${t("Information")}`, t("Whitehat"), t("Cleartrip")],
+    },
+  ];
+
+  // Social Media Icons
+  const socialIcons = [
+    { icon: <FaFacebook />, link: "#" },
+    { icon: <FaTwitter />, link: "#" },
+    { icon: <FaYoutube/>, link: "#" },
+    { icon: <FaInstagram  />, link: "#" },
+  ];
 
   return (
-    <footer className="footer">
-      <div className="footer__container">
+    <footer className="bg-white text-dark py-4">
+      <div className="container">
+        <div className="row g-4">
+          {footerSections.map((section, index) => (
+            <div key={index} className="col-12 col-md-3">
+              <h5>{section.title}</h5>
+              <ul className="list-unstyled">
+                {section.links.map((link, i) => (
+                  <li key={i}>{link}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
 
-        {/* Online Shopping Section */}
-        <div className="footer__section">
-          <h4>{t("ONLINE")} {t("SHOPPING")}</h4>
-          <ul>
-            <li>{t("Men")}</li>
-            <li>{t("Women")}</li>
-            <li>{t("Kids")}</li>
-            <li>{t("Home")} & {t("Living")}</li>
-            <li>{t("Beauty")}</li>
-            <li>{t("Gift")} {t("Cards")}</li>
-            <li>{t("Myntra")} {t("Insider")}</li>
-          </ul>
-        </div>
+          <div className="col-12 col-md-3 text-center text-md-start">
+            <h5>{t("EXPERIENCE")} {t("SHOPSPHERE")} {t("APP")} {t("ON")} {t("MOBILE")}</h5>
+            <div className="d-flex gap-3 justify-content-center justify-content-md-start my-2">
+              <FaGooglePlay className="text-dark" />
+              <FaAppStore className="text-dark" />
+            </div>
 
-        {/* Customer Policies Section */}
-        <div className="footer__section">
-          <h4>{t("CUSTOMER")} {t("POLICIES")}</h4>
-          <ul>
-            <li>
-              <Link className="text-decoration-none text-dark-emphasis" to="/contactus">
-                {t("Contact")} {t("Us")}
-              </Link>
-            </li>
-            <li>{t("FAQ")}</li>
-            <li>{t("Terms")} & {t("Conditions")}</li>
-            <li>{t("Terms")} {t("Of")} {t("Use")}</li>
-            <li>{t("Track")} {t("Orders")}</li>
-            <li>{t("Shipping")}</li>
-            <li>{t("Cancellation")}</li>
-            <li>{t("Returns")}</li>
-            <li>{t("Privacy")} {t("Policy")}</li>
-            <li>{t("Grievance")} {t("Redressal")}</li>
-          </ul>
-        </div>
-
-        {/* Useful Links Section */}
-        <div className="footer__section">
-          <h4>{t("USEFUL")} {t("LINKS")}</h4>
-          <ul>
-            <li>{t("Blog")}</li>
-            <li>{t("Careers")}</li>
-            <li>{t("Site")} {t("Map")}</li>
-            <li>{t("Corporate")} {t("Information")}</li>
-            <li>{t("Whitehat")}</li>
-            <li>{t("Cleartrip")}</li>
-          </ul>
-        </div>
-
-        {/* Mobile App Section */}
-        <div className="footer__section">
-          <h4>{t("EXPERIENCE")} {t("SHOPSPHERE")} {t("APP")} {t("ON")} {t("MOBILE")}</h4>
-          <div className="footer_apps">
-            <FaGooglePlay className="footer_icon" />
-            <FaAppStore className="footer_icon" />
-          </div>
-
-          {/* Social Media Links */}
-          <h4>{t("KEEP")} {t("IN")} {t("TOUCH")}</h4>
-          <div className="footer_socials">
-            <FaFacebook className="footer_icon" />
-            <FaTwitter className="footer_icon" />
-            <FaYoutube className="footer_icon" />
-            <FaInstagram className="footer_icon" />
+            <h5>{t("KEEP")} {t("IN")} {t("TOUCH")}</h5>
+            <div className="d-flex gap-3 justify-content-center justify-content-md-start my-2">
+              {socialIcons.map((item, i) => (
+                <a key={i} href={item.link} className="text-dark">{item.icon}</a>
+              ))}
+            </div>
           </div>
         </div>
 
-        {/* Additional Info Section */}
-        <div className="footer__info">
-          <div>
-            <h4>{t("100%")} {t("ORIGINAL")}</h4>
-            <p>{t("Guaranteed")} {t("for")} {t("all")} {t("products")} {t("at")} {t("ShopSphere.com")}</p>
+        <div className="row mt-4">
+          <div className="col-12 col-md-6 text-center text-md-start">
+            <h6>{t("100%")} {t("ORIGINAL")}</h6>
+            <p className="small">{t("Guaranteed")} {t("for")} {t("all")} {t("products")} {t("at")} {t("ShopSphere.com")}</p>
           </div>
-          <div>
-            <h4>{t("Return")} {t("within")} {t("14")} {t("days")}</h4>
-            <p>{t("of")} {t("receiving")} {t("your")} {t("order")}</p>
+          <div className="col-12 col-md-6 text-center text-md-start">
+            <h6>{t("Return")} {t("within")} {t("14")} {t("days")}</h6>
+            <p className="small">{t("of")} {t("receiving")} {t("your")} {t("order")}</p>
           </div>
         </div>
-
       </div>
     </footer>
   );
